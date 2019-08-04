e135286a8f5900493b0c84b7e79eedd7949eecf7
OmkarDeshpande7 <omideshpande77@gmail.com>
2019-08-05 01:52:31 +0530

    project upload

---
+++ app/src/main/java/com/example/omkar/moderider/activ_2.java
@@ -0,0 +1,19 @@
+package com.example.omkar.moderider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by omkar on 7/20/18.
+ */
+
+public class activ_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activ2);
+    }
+
+}
+
+
